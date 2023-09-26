dfb0c0ab0420148fc314367223ed299c78c24a9e
buminduyasith <bumiduyasith@gmail.com>
2023-09-26 18:17:54 +0530

    create primary button compoent

---
+++ WalletGuard/Components/PrimaryButton.swift
@@ -0,0 +1,28 @@
+//
+//  PrimaryButton.swift
+//  WalletGuard
+//
+//  Created by bumindu yasith on 2023-09-26.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    
+    @State var btnName : String = ""
+    var body: some View {
+        Text(btnName)
+            .foregroundColor(Color.white)
+            .font(.headline)
+            .padding()
+            .padding(.horizontal, 40)
+            .background(Color.btnPrimary)
+            .cornerRadius(15.0)
+    }
+}
+
+struct PrimaryButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PrimaryButton(btnName: "st")
+    }
+}
